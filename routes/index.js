055f709e7f8a898f6da82c19e1aa580c1cc33d90
左盐 <huabinglan@163.com>
2013-12-13 18:28:16 +0800

    添加安全处理，屏蔽掉所有带后缀的请求

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ module.exports=function(app){
       try{
            var upath=req.path,
                urlpath=upath.split('/');
+          if(upath.indexOf('.')>-1){
+              res.send('这是一个错误地址');
+              return;
+          }
            urlpath.shift();
            if(urlpath[urlpath.length-1]==''){urlpath.pop();}
            if(upath=='/'){urlpath=new Array('index','index');}
